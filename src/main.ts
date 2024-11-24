46a9b5f3ff7693ff40b10490e3acabe951de2f8a
Phillip Jhon Lumod <philliplumod@gmail.com>
2024-11-24 22:33:01 +0800

    swagger setup

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ResponseInterceptor } from './common/serializers/responseInterceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const configService = new ConfigService();
 
@@ -10,7 +11,18 @@ const port = configService.get<number>('PORT');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new ResponseInterceptor());
+
+  // Set up Swagger documentation
+  const config = new DocumentBuilder()
+    .setTitle('E-commerce API')
+    .setDescription('API for an e-commerce application')
+    // .addBearerAuth({ in: 'header', type: 'http' }) // Bearer token for authentication
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  //Port to listen on
   await app.listen(port);
-  console.log(`Application is running on: http://localhost:${port}`);
+  console.log(`Application is running on: http://localhost:${port}/api`);
 }
 bootstrap();
