f3348e9e119b25c08f64371088ff880a443cfd5c
Phillip Jhon Lumod <philliplumod@gmail.com>
2024-11-24 22:22:30 +0800

    Add class-validator and update CreateUserDto with validation rules https://youtu.be/wwktn2EafpM?t=3416

--- src/models/users/dto/create-user.dto.ts
+++ src/models/users/dto/create-user.dto.ts
@@ -1,1 +1,25 @@
-export class CreateUserDto {}
+import { IsIn, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { userRole } from '../schema/user.schema';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsIn([userRole.ADMIN, userRole.USER])
+  role: string;
+
+  @IsOptional()
+  @IsString()
+  secretToken?: string;
+}
